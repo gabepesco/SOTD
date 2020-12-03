b7ba0f3908f5acb61f3c21c0220cda7c751d1850
Gabe Pesco <gabepesco@gmail.com>
2020-12-02 20:41:56 -0500

    improved syntax

--- src/main.py
+++ src/main.py
@@ -1,70 +1,67 @@
-import aiocron
-import discord
-from datetime import datetime
-from src import env
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 import os
+import discord
+import spotipy
+import aiocron
+from datetime import datetime
+from spotipy.oauth2 import SpotifyOAuth
+from src.config import *
 
 
-# Discord Bot Initialization
-TOKEN = env.DISCORD_TOKEN
-GUILD = env.DISCORD_GUILD
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
+def main():
+    # Discord Bot Initialization
+    TOKEN = DISCORD_TOKEN
+    GUILD = DISCORD_GUILD
+    intents = discord.Intents.default()
+    intents.members = True
+    client = discord.Client(intents=intents)
 
-# Spotify API Initialization
-os.environ['SPOTIPY_REDIRECT_URI'] = env.SPOTIPY_REDIRECT_URI
-os.environ['SPOTIPY_CLIENT_ID'] = env.SPOTIPY_CLIENT_ID
-os.environ['SPOTIPY_CLIENT_SECRET'] = env.SPOTIPY_CLIENT_SECRET
-auth_manager = SpotifyOAuth(scope='playlist-modify-private')
-sp = spotipy.Spotify(auth_manager=auth_manager)
+    # Spotify API Initialization
+    os.environ['SPOTIPY_REDIRECT_URI'] = SPOTIPY_REDIRECT_URI
+    os.environ['SPOTIPY_CLIENT_ID'] = SPOTIPY_CLIENT_ID
+    os.environ['SPOTIPY_CLIENT_SECRET'] = SPOTIPY_CLIENT_SECRET
+    auth_manager = SpotifyOAuth(scope='playlist-modify-public')
+    sp = spotipy.Spotify(auth_manager=auth_manager)
 
+    @aiocron.crontab("00 22 * * *")
+    async def notify():
+        channel = client.get_channel(CHANNEL_ID)
+        guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
 
-@aiocron.crontab("30 22 * * *")
-async def notify():
-    channel = client.get_channel(env.BOT_CHANNEL_ID)
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+        day = datetime.today().weekday()
+        user_id = DAY_UID_DICT[day]
 
-    day = datetime.today().weekday()
-    user_id = env.DAY_UID_DICT[day]
+        for member in guild.members:
+            if member.id == user_id:
+                user = member
 
-    for member in guild.members:
-        if member.id == user_id:
-            user = member
+        await channel.send(f'Hey {user.mention}, it\'s your song of the day!')
+        print(f"Notified {user}.")
 
-    await channel.send(f'Hey {user.mention}, it\'s your song of the day!')
+    @client.event
+    async def on_message(message):
+        print("on_message")
+        if "https://open.spotify.com/track/" in message.content and message.channel.id == CHANNEL_ID:
+            words = str.split(message.content, " ")
+            for word in words:
+                if "https://open.spotify.com/track/" in word:
+                    url = word
+            track_uri = "spotify:track:" + str.split(url, "?")[0][31:]
+            playlist_uri = SPOTIFY_PLAYLIST_URI
 
+            # add to playlist with spotify API
+            sp.playlist_add_items(playlist_uri, [track_uri])
+            await message.channel.send(f"Added {message.author}'s song to the playlist.")
+            print(f"Added {message.author}'s song to the playlist.")
 
-@client.event
-async def on_message(message):
-    # don't respond to ourselves
-    #print(message.author, client.user)
-    print("on_message")
+    @client.event
+    async def on_ready():
+        guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+        print(f'{client.user} is connected to the following guild:\n'
+              f'{guild.name}(id: {guild.id})')
 
-    if "https://open.spotify.com/track/" in message.content and message.channel.id == env.BOT_CHANNEL_ID:
-        words = str.split(message.content, " ")
-        for word in words:
-            if "https://open.spotify.com/track/" in word:
-                url = word
-        track_uri = "spotify:track:" + str.split(url, "?")[0][31:]
-        playlist_uri = env.SPOTIFY_PLAYLIST_URI
+        notify.start()
 
-        # add to playlist with spotify API
-        sp.playlist_add_items(playlist_uri, [track_uri])
-        await message.channel.send(f"Added {message.author}'s song to the playlist.")
-
-
-@client.event
-async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-    notify.start()
+    client.run(TOKEN)
 
 
-client.run(TOKEN)
+main()
